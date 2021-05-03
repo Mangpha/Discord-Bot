eadca0b8cfe5aa06c96aed66d2958a9d2b55d7cb
b1ind3r <78544031+b1ind3r@users.noreply.github.com>
2021-05-03 21:59:45 +0900

    Delete discord bot options

--- discord_bot.py
+++ discord_bot.py
@@ -22,19 +22,26 @@ async def hello(ctx):
     await ctx.send("Hello")
 
 
-@bot.command()
-async def headtext(ctx):
-    embed = discord.Embed(
-        title="Welcome", description="Testing Bot", color=0xf3bb76)
-    embed.add_field(name="aaaaaa", value="Hello")
-    await ctx.send(embed=embed)
-
-
 @bot.command(help="데스티니 활동 랜덤 추천")
 async def 추천좀(ctx):
-    ran_list = ['레이드', '예언', '선각자', '예지', '이단의 구덩이',
-                '조각난 왕관', '갬빗', '시련의 장', '전장', '선봉대 공격전', '황혼전: 시련', '종료', '슬픔의 제단', '업적작', '탑 공굴리기 & 용암도전']
-    em = discord.Embed(title="데스티니 가디언즈 활동 추천", description="", color=0xf3bb76)
+    ran_list = [
+        "레이드",
+        "예언",
+        "선각자",
+        "예지",
+        "이단의 구덩이",
+        "조각난 왕관",
+        "갬빗",
+        "시련의 장",
+        "전장",
+        "선봉대 공격전",
+        "황혼전: 시련",
+        "종료",
+        "슬픔의 제단",
+        "업적작",
+        "탑 공굴리기 & 용암도전",
+    ]
+    em = discord.Embed(title="데스티니 가디언즈 활동 추천", description="", color=0xF3BB76)
     em.add_field(name="추천 결과", value=random.choice(ran_list))
     await ctx.send(embed=em)
 
@@ -42,7 +49,7 @@ async def 추천좀(ctx):
 @bot.command(help="한강 수온 체크")
 async def 한강물온도(ctx):
     hangang_api = "https://api.hangang.msub.kr/"
-    embed = discord.Embed(title="한강 물 온도 체크", description="", color=0xf3bb76)
+    embed = discord.Embed(title="한강 물 온도 체크", description="", color=0xF3BB76)
     res = requests.get(hangang_api)
     res_json = json.loads(res.text)
     temp = res_json["temp"]
