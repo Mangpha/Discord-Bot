75988b9d4f1ef8022eb4d8a31d1ef0b16aa7e432
Mangpha <8350130@gmail.com>
2021-05-23 18:43:12 +0900

    Checking number

--- games/games.py
+++ games/games.py
@@ -1,4 +1,5 @@
 import random
+from . import user_sql
 
 
 def make_lotto():
@@ -30,3 +31,30 @@ def users_lotto(args):
 
     except TypeError:
         return False
+
+
+def check_lotto(user, bots, userid):
+    check = 0
+    for i in range(0, len(bots)):
+        if user[i] in bots:
+            check += 1
+
+    # apply n times to return value
+    if check == 2:
+        return 1
+
+    if check == 3:
+        return 2
+
+    if check == 4:
+        return 5
+
+    if check == 5:
+        return 10
+
+    if check == 6:
+        return 100
+
+    else:
+        user_sql.set_data(userid, "LostMoney", 500)
+        return 0
