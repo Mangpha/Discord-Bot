2beac526edb205da526f73c05ea394303e023109
Mangpha <8350130@gmail.com>
2021-05-23 19:25:03 +0900

    Args: List => Str

--- discord_bot.py
+++ discord_bot.py
@@ -330,7 +330,6 @@ async def 로또(ctx, option="도움", *, user_lotto=""):
             await ctx.send(embed=embed)
 
         if option == "구매":
-            print(user_lotto)
             user_lotto = games.users_lotto(list(map(int, user_lotto.split())))
             bots_lotto = games.make_lotto()
             if user_lotto is False:
